78007fe940f8d07bade65a419b6f5de0c966da96
nayhoy77้ <flukrattanachot@gmail.com>
2024-09-11 12:10:16 +0700

    first commit

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes_1 = require("./routes");
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// applying the routes to the basepath '/api'
+app.use('/api', routes_1.default);
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('konesaan');
+});
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
+// http://localhost:3000/api/products
---
+++ src/db/product.ts
@@ -0,0 +1,17 @@
+
+import { RowDataPacket } from "mysql2"; // Import type for rows returned from queries
+import { promisePool } from "../config/db";
+import { Request, Response } from "express"; // Import Request and Response from express
+
+const selectAll = async () => {
+  try {
+    const [rows]: [RowDataPacket[], any] = await promisePool.query(
+      "SELECT * FROM product"
+    );
+    return rows;
+  } catch (err) {
+    console.error("Database query error:", err);
+  }
+};
+
+export default { selectAll };
